a8925a537b2ec2a9f16256162a70ffec59674cb7
beyamor <beyamor@gmail.com>
2013-10-02 15:07:50 -0700

    Using array2d in level

--- src/demo/level.cljs
+++ src/demo/level.cljs
@@ -1,14 +1,15 @@
 (ns demo.level
-  (:require [demo.tiles :as ts])
+  (:require [demo.tiles :as ts]
+            [ruin.array2d :as a2d])
   (:require-macros [lonocloud.synthread :as ->]))
 
 (defn get-tile
   [level x y]
-  (aget (aget (:tiles level) x) y))
+  (a2d/get (:tiles level) x y))
 
 (defn set-tile
   [level x y tile]
-  (aset (aget (:tiles level) x) y tile)
+  (a2d/set! (:tiles level) x y tile)
   level)
 
 (defn dig
