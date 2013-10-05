c557d907b7d59c52f12031f47cf9939b7f016b86
beyamor <beyamor@gmail.com>
2013-10-04 22:01:51 -0700

    Selecting in the inventory screen

--- src/demo/screens.cljs
+++ src/demo/screens.cljs
@@ -8,7 +8,7 @@
 
 (def number-of-items (comp count :items :entity))
 
-(defn inventory-index-i
+(defn nth-inventory-index
   [state i]
   (nth (-> state :entity :items keys) i))
 
@@ -17,28 +17,47 @@
   (doto display
     (d/draw-text! 0 0 "Inventory"))
   (dotimes [i (number-of-items state)]
-    (let [index (inventory-index-i state i)
+    (let [index (nth-inventory-index state i)
           item (get-in state [:entity :items index])]
-      (print index (get-in state [:entity :items]))
-      (doto display
-        (d/draw-text! 0 (+ 2 i)
-                      (str (int->letter i) " " (i/describe item)))))))
+      (->>
+        [(int->letter i)
+         (if (= (:selection state) i) "+" "-")
+         (i/describe item)]
+        (interpose " ")
+        (apply str)
+        (d/draw-text! display 0 (+ 2 i))))))
 
 (defn initial-inventory-state
   [entity]
   {:entity entity
    :selection 0})
 
+(defn set-selection
+  [state index]
+  (->>
+    (loop [selection 0]
+      (if (= (nth-inventory-index state selection) index)
+        selection
+        (recur (inc selection))))
+    (assoc state :selection)))
+
 (defn inventory
   [entity display key-events]
   (let [state (initial-inventory-state entity)]
-    (print (number-of-items state))
     (render-inventory state display)
     (go
-      (loop [[event-type key-code] (<! key-events)]
+      (loop [[event-type key-code] (<! key-events) state state]
         (cond
           (and (= :down event-type) (= key-code js/ROT.VK_ESCAPE))
           nil
 
+          (and (>= key-code js/ROT.VK_A) (<= key-code js/ROT.VK_Z))
+          (let [index (- key-code js/ROT.VK_A)]
+            (if (contains? (-> state :entity :items) index)
+              (let [updated-state (set-selection state index)]
+                (render-inventory updated-state display)
+                (recur (<! key-events) updated-state))
+              (recur (<! key-events) state)))
+
           :else
-          (recur (<! key-events)))))))
+          (recur (<! key-events) state))))))
